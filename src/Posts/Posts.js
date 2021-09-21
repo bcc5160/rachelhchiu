d70134787965dc5bd6f404517bb02321a134e059
Brittany Chiu <bc.chiu12@gmail.com>
2021-09-21 18:20:33 -0400

    react site init and nav

---
+++ src/Posts/Posts.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+import "./Posts.css";
+
+function Posts() {
+    return(
+        <div className="Posts">
+            <Container>
+                <h1 class="posts-title">Articles</h1>
+                <div class="bottom-border"></div>
+            </Container>
+            <Container>
+    
+            </Container>
+        </div>
+    );
+}
+
+export default Posts;
